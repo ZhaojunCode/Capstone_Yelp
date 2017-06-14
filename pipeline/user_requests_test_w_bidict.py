a1a04ad61245524d1a16d7334c49e27dd6f3d915
rezaisrad <rezarad77@gmail.com>
2017-06-14 17:08:54 -0400

    added user stream test

--- pipeline/user_requests_test_w_bidict.py
+++ pipeline/user_requests_test_w_bidict.py
@@ -2,21 +2,23 @@ from kafka import KafkaProducer
 import csv
 import json
 import time
-from datetime import datetime
 
 # Connect to Kafka
-producer = KafkaProducer(bootstrap_servers='ec2-54-173-251-232.compute-1.amazonaws.com:9092',
-                                                value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers='ec2-54-173-251-232.compute-1.amazonaws.com:9092')
 topic = 'sparktest'
 
 with open('../data/IUC-businesses.csv', mode='r') as infile:
+    print "loop 1"
     reader = csv.reader(infile)
     mydict  = {}
     for rows in reader:
+        print "loop 2"
         mydict['id'] = rows[0]
         mydict['name'] = rows[1]
         mydict['longitude'] = rows[2]
         mydict['latitude'] = rows[3]
-        producer.send(topic, mydict)
-        print "Sent {0}".format(mydict)
+        producer.send(topic, b'{0}'.format(mydict))
+        print "end of loop 2"
         time.sleep(2)
+
+print 'finished'
